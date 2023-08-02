3b2c0e02f114a242d54859b9d5b75dd380318ade
Sam Robinson <sam.robinson@smoothwall.com>
2023-08-02 12:08:45 +0100

    putting albums back to just three in the starting state

--- controllers/getAlbums.go
+++ controllers/getAlbums.go
@@ -11,10 +11,7 @@ import (
 var Albums = []album.Album{
 	{ID: "123", Title: "Hot Fuss", Artist: "The Killers", Price: 17.99},
 	{ID: "321", Title: "The Dark Side Of The Moon ", Artist: "Pink Floyd", Price: 39.99},
-	{ID: "567", Title: "some new album", Artist: "AC/DC", Price: 56.99},
-	{ID: "680", Title: "another album", Artist: "AC/DC", Price: 56.99},
-	{ID: "701", Title: "one more album", Artist: "AC/DC", Price: 56.99},
-	{ID: "100", Title: "some other album", Artist: "AC/DC", Price: 56.99},
+	{ID: "567", Title: "Highway To Hell", Artist: "AC/DC", Price: 56.99},
 }
 
 func GetAllAlbums(c *gin.Context) {
